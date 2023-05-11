e3a10e20459bec8232d7a9ae3cd447eee6e49929
Smiboi <128560300+Smiboi@users.noreply.github.com>
2023-05-11 21:34:58 +0300

    part3 esim.sovellus v2

--- part3/models/note.js
+++ part3/models/note.js
@@ -1,26 +1,10 @@
 const mongoose = require('mongoose')
 
-mongoose.set('strictQuery', false)
-
-// const url =
-//   `mongodb+srv://fullstack:${password}@fullstackopen.y9leolb.mongodb.net/noteApp?retryWrites=true&w=majority`
-const url = process.env.MONGODB_URI
-
-console.log('connecting to', url)
-
-mongoose.connect(url)
-  .then(() => {
-    console.log('connected to MongoDB')
-  })
-  .catch((error) => {
-    console.log('error connecting to MongoDB:', error.message)
-  })
-
 const noteSchema = new mongoose.Schema({
   content: {
     type: String,
-    minlength: 5,
-    required: true
+    required: true,
+    minlength: 5
   },
   important: Boolean,
 })
