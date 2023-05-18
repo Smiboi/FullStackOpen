3743106735e54f67ccf6dd24fedfc0c5b479dcf8
Smiboi <128560300+Smiboi@users.noreply.github.com>
2023-05-18 04:08:09 +0300

    osa2 esimerkkisovellus hajosi

--- osa2/src/components/LoginForm.js
+++ osa2/src/components/LoginForm.js
@@ -1,3 +1,5 @@
+import PropTypes from 'prop-types'
+
 const LoginForm = ({
   handleSubmit,
   handleUsernameChange,
@@ -24,12 +26,19 @@ const LoginForm = ({
             value={password}
             onChange={handlePasswordChange}
           />
-      </div>
+        </div>
         <button type="submit">login</button>
       </form>
     </div>
   )
 }
 
+LoginForm.propTypes = {
+  handleSubmit: PropTypes.func.isRequired,
+  handleUsernameChange: PropTypes.func.isRequired,
+  handlePasswordChange: PropTypes.func.isRequired,
+  username: PropTypes.string.isRequired,
+  password: PropTypes.string.isRequired
+}
+
 export default LoginForm
- 
